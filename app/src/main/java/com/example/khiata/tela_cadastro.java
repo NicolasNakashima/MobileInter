57fd82feab59eae36d1b1b448e798536557c7757
HeitorCarvalhoCampos <heitor.campos@germinare.org.br>
2024-09-08 01:01:43 -0300

    Adicionando mais navegações iniciais e refatorando tela de cadastro

--- app/src/main/java/com/example/khiata/tela_cadastro.java
+++ app/src/main/java/com/example/khiata/tela_cadastro.java
@@ -1,6 +1,9 @@
 package com.example.khiata;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class tela_cadastro extends AppCompatActivity {
+
+    ImageView cadastro_voltar_inicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class tela_cadastro extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        cadastro_voltar_inicio = findViewById(R.id.cadastro_voltar_inicio);
+        cadastro_voltar_inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(tela_cadastro.this, tela_inicial.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/khiata/tela_nova_senha.java
+++ app/src/main/java/com/example/khiata/tela_nova_senha.java
@@ -1,6 +1,10 @@
 package com.example.khiata;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class tela_nova_senha extends AppCompatActivity {
+
+    ImageView voltar_login;
+    Button btn_atualizar_senha, btn_cancelar_atualizar_senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,24 @@ public class tela_nova_senha extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        voltar_login = findViewById(R.id.voltar_login);
+        voltar_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(tela_nova_senha.this, tela_login.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_cancelar_atualizar_senha = findViewById(R.id.btn_cancelar_atualizar_senha);
+        btn_cancelar_atualizar_senha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(tela_nova_senha.this, tela_login.class);
+                startActivity(intent);
+            }
         });
     }
 }
