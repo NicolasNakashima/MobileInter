59c91394d99a6fc599c6b7fc9278fc2ad9f27c76
HeitorCarvalhoCampos <heitor.campos@germinare.org.br>
2024-10-02 10:43:28 -0300

    Navegação do menu lateral

--- app/src/main/java/com/example/khiata/classes/MainActivity.java
+++ app/src/main/java/com/example/khiata/classes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.khiata.classes;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,10 +21,17 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.khiata.R;
+import com.example.khiata.fragments.fragment_tela_avaliacoes;
+import com.example.khiata.fragments.fragment_tela_cursos;
+import com.example.khiata.fragments.fragment_tela_enderecos;
 import com.example.khiata.fragments.fragment_tela_home;
 import com.example.khiata.fragments.fragment_tela_perfil;
 import com.example.khiata.fragments.fragment_tela_compras;
 import com.example.khiata.fragments.fragment_tela_favoritos;
+import com.example.khiata.fragments.fragment_tela_statistics;
+import com.example.khiata.fragments.fragment_tela_area_costureira;
+import com.example.khiata.fragments.fragment_tela_premium_plan;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +39,15 @@ public class MainActivity extends AppCompatActivity {
     private fragment_tela_home fragment_tela_home = new fragment_tela_home();
     private fragment_tela_compras fragment_tela_compras = new fragment_tela_compras();
     private fragment_tela_perfil fragment_tela_perfil = new fragment_tela_perfil();
+    private fragment_tela_cursos fragment_tela_cursos = new fragment_tela_cursos();
+    private fragment_tela_statistics fragment_tela_statistics = new fragment_tela_statistics();
+    private fragment_tela_area_costureira fragment_tela_area_costureira = new fragment_tela_area_costureira();
+    private fragment_tela_enderecos fragment_tela_enderecos = new fragment_tela_enderecos();
+    private fragment_tela_avaliacoes fragment_tela_avaliacoes = new fragment_tela_avaliacoes();
+    private fragment_tela_premium_plan fragment_tela_premium_plan = new fragment_tela_premium_plan();
     ImageView btn_lateral_menu, btn_navigation_favoritos, btn_navigation_home, btn_navigation_compras, btn_navigation_perfil;
+    View navigation_perfil, navigation_cursos, navigation_statistics, navigation_area_costureira, navigation_enderecos, navigation_avaliacoes, btn_logout;
+    Button btn_adquirir_premium;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,6 +118,105 @@ public class MainActivity extends AppCompatActivity {
                 Dialog dialog = new Dialog(MainActivity.this);
                 LayoutInflater inflater = getLayoutInflater();
                 View menu_lateral = inflater.inflate(R.layout.menu_lateral, null);
+
+                //Ir para tela de perfil
+                navigation_perfil = menu_lateral.findViewById(R.id.navigation_perfil);
+                navigation_perfil.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_conteudo, fragment_tela_perfil);
+                        transaction.commit();
+                        dialog.cancel();
+                    }
+                });
+
+                //Ir para tela de cursos
+                navigation_cursos = menu_lateral.findViewById(R.id.navigation_cursos);
+                navigation_cursos.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_conteudo, fragment_tela_cursos);
+                        transaction.commit();
+                        dialog.cancel();
+                    }
+                });
+
+                //Ir para tela de estatísticas
+                navigation_statistics = menu_lateral.findViewById(R.id.navigation_statistics);
+                navigation_statistics.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_conteudo, fragment_tela_statistics);
+                        transaction.commit();
+                        dialog.cancel();
+                    }
+                });
+
+                //Ir para tela da área de costureira
+                navigation_area_costureira = menu_lateral.findViewById(R.id.navigation_area_costureira);
+                navigation_area_costureira.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_conteudo, fragment_tela_area_costureira);
+                        transaction.commit();
+                        dialog.cancel();
+                    }
+                });
+
+                //Ir para tela de endereços
+                navigation_enderecos = menu_lateral.findViewById(R.id.navigation_enderecos);
+                navigation_enderecos.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_conteudo, fragment_tela_enderecos);
+                        transaction.commit();
+                        dialog.cancel();
+                    }
+                });
+
+                //Ir para tela de avaliações
+                navigation_avaliacoes = menu_lateral.findViewById(R.id.navigation_avaliacoes);
+                navigation_avaliacoes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_conteudo, fragment_tela_avaliacoes);
+                        transaction.commit();
+                        dialog.cancel();
+                    }
+                });
+
+                //Ir para tela do plano premium
+                btn_adquirir_premium = menu_lateral.findViewById(R.id.btn_adquirir_premium);
+                btn_adquirir_premium.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_conteudo, fragment_tela_premium_plan);
+                        transaction.commit();
+                        dialog.cancel();
+                    }
+                });
+
+                //Logout do App
+                btn_logout = menu_lateral.findViewById(R.id.btn_logout);
+                btn_logout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(MainActivity.this, tela_inicial.class);
+                        startActivity(intent);
+                        finish();
+                        dialog.cancel();
+                    }
+                });
+
+
                 dialog.setContentView(menu_lateral);
                 dialog.setCancelable(true);
 
